3241c8ca4df4d2abd4bc73ce7fb16eb9e24838ae
Your Name <you@example.com>
2023-03-18 15:14:13 +0100

    edycja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  email: string = '';
+  password: string = '';
+  repeated_password: string = '';
+  message: string = '';
+
+  onSubmit(){
+    // this.message = `działa`;
+    if (!this.email.includes('@')){
+      this.message=`Nieprawidłowy adres e-mail: ${this.email}`;
+      return;
+    }
+    if (this.password!==this.repeated_password){
+      this.message=`Hasła różnią się.`;
+      return;
+    }
+    this.message=`Witaj ${this.email}`;
+  }
 }
+
+
+
+// formularz zgodnie z bootstrapem
